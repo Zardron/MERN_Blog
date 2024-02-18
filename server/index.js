65d7954709a808a8371181794edaa3df52cc0850
Zardron <zardron@clockworkdelivery.com>
2024-02-18 20:47:39 +0400

    create database connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,3 +9,8 @@ const app = express();
 app.listen(3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
